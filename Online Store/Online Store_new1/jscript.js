da91fe69a7ef60b7dfa8ce862450b671aea4b5b8
99simran <48862968+99simran@users.noreply.github.com>
2020-08-30 23:30:30 +0530

    Add files via upload

---
+++ Online Store/Online Store_new1/jscript.js
@@ -0,0 +1,13 @@
+function upDate(){
+	document.getElementById("items").innerHTML="for a 3D image , click here!";
+}
+function unDo(){
+	document.getElementById("items").innerHTML="";
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
